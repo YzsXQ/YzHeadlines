b2d45924a610ba8f24dd1e34a1f876b51a579296
yizhi <yizhi@cdhexinyi.com>
2015-12-31 11:39:57 +0800

    CreateYzHeadlines

---
+++ YzHeadlines/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YzHeadlines
+//
+//  Created by 易志 on 15/12/31.
+//  Copyright © 2015年 yzs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
